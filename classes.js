1f4793a4d200783a4402f9f5ae0b229362b9a243
Axumawityemaneh <yemanehaxumawit@gmail.com>
2024-04-25 07:07:47 +0300

    add files

--- classes.js
+++ classes.js
@@ -1,58 +1,110 @@
-class Question {
-    constructor() {
-      this.questions = [];
-      this.currentQuestionIndex = 0;
-      this.score = 0;
+class Car{
+    constructor(make,model,year,isAvailable){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.isAvailable =isAvailable
     }
-  
-    addQuestion(questionObj) {
-      this.questions.push(questionObj);
+    toggleAvailability(){
+        console.log(`The ${this.make} is not available for rent`)
     }
-  
-    nextQuestion() {
-      if (this.currentQuestionIndex < this.questions.length - 1) {
-        this.currentQuestionIndex++;
-      } else {
-        console.log("End of quiz");
-      }
+}
+const car = new Car("toyota","camry",2020,"The car is available for rent")
+console.log(car.toggleAvailability());
+class Rental extends Car{
+    constructor(make,model,year,isAvailable,rentalName,rentalStartDate,rentalEndDate){
+        super(make,model,year,isAvailable)
+        this.rentalName=rentalName
+        this.rentalStartDate=rentalStartDate
+        this.rentalEndDate=rentalEndDate
     }
-  
-    submitAnswer(userAnswer) {
-      const currentQuestion = this.questions[this.currentQuestionIndex];
-      if (currentQuestion.checkAnswer(userAnswer)) {
-        this.score++;
-        console.log("Correct answer!");
-      } else {
-        console.log("Wrong answer!");
-      }
+    calculateRentalDuration(){
+        console.log(`The rental duration is${this.rentalEndDate-this.rentalStartDate}`)
     }
-  }
-  
-  class QuestionObject {
-    constructor(question, answer) {
-      this.question = question;
-      this.answer = answer;
+}
+const rentalCar = new Rental("toyota","camry",2020,"The car is available for rent","Rental Households",20,30)
+console.log(rentalCar.make)
+rentalCar.calculateRentalDuration()
+class Question{
+    constructor(text,options,correctAnswer){
+        this.text = text
+        this.options = options
+        this.correctAnswer=correctAnswer
     }
-  
-    checkAnswer(userAnswer) {
-      return userAnswer.toLowerCase() === this.answer.toLowerCase();
+    checkAnswer(answer){
+        this.answer=answer
+        if (`${this.answer}`!==`${this.correctAnswer}`) {
+            console.log("false");
+        }
+        else{
+            console.log("true");
+        }
     }
-  }
-  
-  // Example usage:
-  const quiz = new Question();
 
-  const question1 = new QuestionObject("What do you prefer ?", "coffee");
-  const question2 = new QuestionObject("Do you love javascript?", "Yes");
-  
-  quiz.addQuestion(question1);
-  quiz.addQuestion(question2);
-  
-  quiz.submitAnswer("Paris"); // Correct answer!
-  quiz.nextQuestion();
-  quiz.submitAnswer("Mars"); //
-  
-  //Questuion2
+}
+const firstQuestion = new Question("How many planets do we have?",[1,2,3,4,5,6,7,8],8)
+console.log({firstQuestion});
+console.log(firstQuestion.text);
+console.log(firstQuestion.options);
+console.log(firstQuestion.correctAnswer);
+firstQuestion.checkAnswer(3)
+firstQuestion.checkAnswer(8)
+const secondQuestion = new Question("How many countries are in Kenya?",[47,65,23,12],47)
+console.log({secondQuestion});
+const thirdQuestion = new Question("What is the capital city of Kenya?",["Nairobi","Mombasa","Kisumu","Nakuru"],"Nairobi")
+console.log({thirdQuestion});
+const fourthQuestion = new Question("Who is the president of Kenya",["Uhuru","Raila","Mutahi","Obama"],"Uhuru")
+console.log({fourthQuestion});
+const lastQuestion = [fourthQuestion]
+console.log(lastQuestion);
+class Quiz{
+    constructor(questions,index,score){
+        this.questions = questions
+        this.index = index
+        this.score = score
+    }
+    addQuestion(lastQuestion){
+        this.lastQuestion = lastQuestion
+        const allFourQuestions = this.questions.concat(this.lastQuestion)
+        console.log({allFourQuestions});
+    }
+    nextQuestion(){
+        const index = `${this.questions}`
+        for (let index of `${this.questions}`) {
+            index+=`$this.questions`
+            return index
+    }
+    // submitAnswer(){
+    // }
+}
+submitAnswer(answer){
+    `${this.answer}`=answer
+    `${this.score }`= score
+    if (`${this.answer}`!==`${this.correctAnswer}`) {
+        score++;
+        return score
+    }
+    else{
+        return "wrong choice"
+    }
+}
+}
+const quiz = new Quiz([firstQuestion,secondQuestion,thirdQuestion],2,30)
+console.log({quiz});
+quiz.addQuestion(lastQuestion)
+quiz.nextQuestion()
+quiz.submitAnswer(12)
 
-  
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
